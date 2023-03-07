851c2800185eae26868792b5f3822cd2fa2aa878
Mateusz <mateusz.rusiecki@amsterdam-standard.pl>
2023-03-07 17:01:46 +0100

    update bot message handlers

--- src/controller/command.ts
+++ src/controller/command.ts
@@ -3,7 +3,7 @@ import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 import { Router } from "express";
 import '../config/db';
-import { addTodo } from '../repository/todo';
+import { addTodo, findTodos, deleteTodo, deleteAllTodos } from '../repository/todo';
 
 dotenv.config();
 
@@ -24,8 +24,41 @@ bot.onText(/Start/, (msg) => {
 });
 
 bot.onText(/Add/, (msg) => {
-    addTodo(msg.chat.id, msg.text).then(resp => {
-        bot.sendMessage(msg.chat.id, 'Dodano zadanie');
+    findTodos(msg.chat.id).then(resp => {
+        addTodo(msg.chat.id, resp.length + 1, (msg.text || "").replace('\Add ', '')).then(resp => {
+            bot.sendMessage(msg.chat.id, 'Dodano zadanie');
+        }).catch(err => {
+            bot.sendMessage(msg.chat.id, 'Coś poszło nie tak');
+        });
+    });
+});
+
+bot.onText(/List/, (msg) => {
+    findTodos(msg.chat.id).then(resp => {
+        let tasks = "";
+        resp.forEach((todo: any) => {
+            const status = todo.done ? '[done]' : '[todo]';
+            tasks += `[${todo.taskId}] ${status} ${todo.task} \n`;
+        });
+        bot.sendMessage(msg.chat.id, tasks);
+    }).catch(err => {
+        bot.sendMessage(msg.chat.id, 'Coś poszło nie tak');
+    });
+});
+
+bot.onText(/Delete/, (msg) => {
+    const taskNr: string = (msg.text || "").replace( /^\D+/g, '');
+    deleteTodo(msg.chat.id, parseInt(taskNr)).then(resp => {
+        bot.sendMessage(msg.chat.id, 'Usunięto zadanie');
+    }).catch(err => {
+        console.log(err);
+        bot.sendMessage(msg.chat.id, 'Coś poszło nie tak');
+    });
+});
+
+bot.onText(/Del all/, (msg) => {
+    deleteAllTodos(msg.chat.id).then(resp => {
+        bot.sendMessage(msg.chat.id, 'Usunięto wszystkie zadania');
     }).catch(err => {
         bot.sendMessage(msg.chat.id, 'Coś poszło nie tak');
     });
