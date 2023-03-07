b33954bc62b93e2cecaf4f2b277cd799e7c43987
Mateusz <mateusz.rusiecki@amsterdam-standard.pl>
2023-03-07 16:59:26 +0100

    update model structure

--- src/model/todo.ts
+++ src/model/todo.ts
@@ -5,19 +5,20 @@ const TodoSchmea = new mongoose.Schema({
     uid: {
         type: Number,
     },
-    tasks: [{
-        task: {
-            type: String,
-            minLength: [3, "Za którka treść, podaj min. 3 znaki"],
-            maxLength: [255, "Za długa treść, podaj max. 255 znaki"]
-        },
-        done: {
-            type: Boolean,
-        },
-        created: {
-            type: Date,
-        }
-    }]
+    taskId: {
+        type: Number,
+    },
+    task: {
+        type: String,
+        minLength: [3, "Za którka treść, podaj min. 3 znaki"],
+        maxLength: [255, "Za długa treść, podaj max. 255 znaki"]
+    },
+    done: {
+        type: Boolean,
+    },
+    created: {
+        type: Date,
+    }
 });
 
 export default mongoose.model("Todo", TodoSchmea);
