27aa098cf73e9270904d1572b1baf14626f5536a
Mateusz <mateusz.rusiecki@amsterdam-standard.pl>
2023-03-07 17:00:43 +0100

    update addTodo in repository add findTodos, deleteTodo, deleteAllTodos in repository

--- src/repository/todo.ts
+++ src/repository/todo.ts
@@ -3,16 +3,35 @@ import Todo from '../model/todo';
 
 const noTaskMessage = 'Nie masz żadnych zadań.\n Wpisz add <tresc zadania> aby dodać zadanie';
 
-const addTodo = async (uid: string, task: string) => {
+const addTodo = async (uid: Number, taskId: Number, task: string) => {
     const newTodo = new Todo({
         uid,
-        tasks: [{
-            task,
-            done: false,
-            created: new Date()
-        }]
+        taskId,
+        task,
+        done: false,
+        created: new Date()
     });
     await newTodo.save();
 }
 
-export { addTodo }
+const findTodos = async (uid: Number) => {
+    return await Todo.find({ uid });
+};
+
+const deleteTodo = async (uid: Number, taskId: Number) => {
+    try {
+        await Todo.findOneAndDelete({ uid, taskId });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const deleteAllTodos = async (uid: Number) => {
+    try {
+        await Todo.deleteMany({ uid });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export { addTodo, findTodos, deleteTodo, deleteAllTodos }
